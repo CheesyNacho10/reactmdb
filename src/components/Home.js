1e17073e25448321ddd3fea98f5fe239bc6f5f1b
nacho <ndiez@equipson.es>
2023-10-23 12:16:34 +0200

    Made a searchbar and multiple page loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,8 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
+import Button from "./Button";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -18,8 +20,16 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch();
-    console.log(state);
+    const { 
+        state, 
+        loading, 
+        error, 
+        searchTerm, 
+        setSearchTerm, 
+        setIsLoadingMore 
+    } = useHomeFetch();
+
+    if (error) return <div>Something went wrong...</div>;
   
     return (
         <>
@@ -30,7 +40,8 @@ const Home = () => {
                     text={state.results[0].overview}
                 />
             ) : null}
-            <Grid header="Popular Movies">
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
                 {state.results.map(movie => (
                     <Thumb
                         key={movie.id}
@@ -44,7 +55,10 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-            <Spinner />
+            {loading && <Spinner />}
+            {state.page < state.total_pages && !loading && (
+                <Button text="Load More" callback={() => setIsLoadingMore(true)}/>
+            )}
         </>
     )
 };
